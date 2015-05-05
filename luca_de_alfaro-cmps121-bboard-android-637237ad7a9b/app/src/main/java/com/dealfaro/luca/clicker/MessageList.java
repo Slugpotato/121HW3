eb67d0499620cba97dcccdec4ae39b7017ef6222
Andrew Lee <leeandrew781@gmail.com>
2015-05-05 11:13:23 -0700

    Starting point is HW2, working at this point

---
+++ luca_de_alfaro-cmps121-bboard-android-637237ad7a9b/app/src/main/java/com/dealfaro/luca/clicker/MessageList.java
@@ -0,0 +1,12 @@
+package com.dealfaro.luca.clicker;
+
+/**
+ * Created by luca on 14/4/2015.
+ */
+public class MessageList {
+    public MessageList() {};
+    public MsgInfo[] messages;
+
+
+}
+
---
+++ luca_de_alfaro-cmps121-bboard-android-637237ad7a9b/app/src/main/java/com/dealfaro/luca/clicker/MsgInfo.java
@@ -0,0 +1,14 @@
+package com.dealfaro.luca.clicker;
+
+/**
+ * Created by ilaforces1 on 4/28/15.
+ */
+public class MsgInfo {
+    public MsgInfo() {};
+
+    String msg;
+    String dest;
+    String userid;
+    String msgid;
+    String ts;
+}
